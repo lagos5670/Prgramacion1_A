72810a4071b56a279e11936a34c05679c40b3542
Brayan lagos <80788479+lagos5670@users.noreply.github.com>
2023-06-26 19:26:38 -0500

    Actualizacion de toso los proyectos

---
+++ CursoOracleLogica/Encriptador/index.js
@@ -0,0 +1,26 @@
+/*var contador = 0;
+var palabra  = prompt("Ingresar el texto");
+var contador ;
+//este for no ayuda acontar la cantida de palabaras que hay en el arreglo
+for(contador = 0; contador<=palabra.length; contador++){
+    //este if no ayuda hacer la comparacion con el contador y la letra no la cambiara por letra clave 
+     if(palabra.charAt(contador) === "e"){
+        document.write("enter");
+
+    }else if(palabra.charAt(contador) === "i"){
+        document.write("imes");
+
+    }else if(palabra.charAt(contador) === "a"){
+        document.write("ai");
+
+    }else  if(palabra.charAt(contador) === "o"){
+        document.write("ober");
+
+    }else if(palabra.charAt(contador) === "u"){
+        document.write("ufat"); 
+
+    }else{
+        document.write(palabra.charAt(contador));
+    }
+}*/
+        
